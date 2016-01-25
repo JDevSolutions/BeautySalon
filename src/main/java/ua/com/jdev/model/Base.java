42a63d14089efa371eba71406ca1dd940e5f0942
driversti <LPFaint1992>
2016-01-25 14:35:52 +0200

    Добавлено новое поле StringProperty id (проверить, возможно сделать просто String или int) и геттеры

--- src/main/java/ua/com/jdev/model/Base.java
+++ src/main/java/ua/com/jdev/model/Base.java
@@ -1,4 +1,16 @@
 package ua.com.jdev.model;
 
+import javafx.beans.property.StringProperty;
+
 public abstract class Base {
+
+    private StringProperty id;
+
+    public String getId() {
+        return id.get();
+    }
+
+    public StringProperty idProperty() {
+        return id;
+    }
 }
