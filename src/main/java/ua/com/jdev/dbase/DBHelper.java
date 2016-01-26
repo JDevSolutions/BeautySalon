195d6a1d24e674c4f257d091d4073b4497c3ed50
Jurrio <jurimik@yandex.ua>
2016-01-26 19:26:54 +0200

    add logging in DBHelper, create DBRepository

--- src/main/java/ua/com/jdev/dbase/DBHelper.java
+++ src/main/java/ua/com/jdev/dbase/DBHelper.java
@@ -3,11 +3,10 @@ package ua.com.jdev.dbase;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import org.apache.log4j.Level;
 import ua.com.jdev.model.*;
 
-import java.sql.*;
-import java.util.logging.Logger;
-
+import org.apache.log4j.Logger;
 
 /**
  * Created by Yurii Mikhailichenko on 17.01.2016.
@@ -16,16 +15,10 @@ import java.util.logging.Logger;
  */
 
 public class DBHelper {
-    private static final String URL = "jdbc:mysql://localhost:3306";
-    private static final String USER = "iris";
-    private static final String PASSWORD = "1234qaz";
 
-    private static Connection connection;
-    private static Statement statement;
-    private static ResultSet resultSet;
+    private static DBRepository dbRep = new DBRepository();
+
     private static ObservableList<? extends Base> outcomingData;
-
-    private static Logger log = Logger.getLogger(DBHelper.class.getName());
 
     public static final String DATABASE_NAME = "iris_db";
     public static final String TABLE_EMPLOYEES = "employees";
@@ -35,9 +28,11 @@ public class DBHelper {
     public static final String TABLE_CLIENTS = "clients";
     public static final String TABLE_CARDS = "cards";
 
+    private final static Logger log = Logger.getLogger(DBHelper.class);
+
     public static ObservableList<? extends Base> getData(String tableName) {
         String query = "SELECT * FROM " + tableName + ";";
-        outcomingData = executeQuery(query, tableName);
+        outcomingData = dbRep.executeQuery(query, tableName);
         return outcomingData;
     }
 
@@ -47,8 +42,8 @@ public class DBHelper {
                 appendRequiredField(client.getFirstName()) + ", " + appendField(client.getSecondName()) + ", " +
                 appendRequiredField(client.getLastName()) + ", " + appendField(client.getPhone()) + ", " +
                 appendField(client.getCardNumber()) + ");");
-        log.info("build query " + query.toString());
-        executeUpdate(query.toString());
+        log.log(Level.INFO, query.toString());
+        dbRep.executeUpdate(query.toString());
     }
 
     public static void insert(Goods goods)  {
@@ -56,7 +51,8 @@ public class DBHelper {
                 appendRequiredField(goods.getId()) + ", " + appendRequiredField(goods.getCode()) + ", " +
                 appendRequiredField(goods.getName()) + ", " + appendField(goods.getPrice()) + ", " +
                 appendField(goods.getAmount()));
-        executeUpdate(query.toString());
+        log.log(Level.INFO, query.toString());
+        dbRep.executeUpdate(query.toString());
     }
 
     public static void insert(Employee employee)  {
@@ -64,7 +60,8 @@ public class DBHelper {
                 appendRequiredField(employee.getId()) + ", " + appendRequiredField(employee.getFirstName()) + ", " +
                 appendField(employee.getSecondName()) + ", " + appendRequiredField(employee.getLastName()) + ", " +
                 appendField(employee.getPhone()) + appendRequiredField(employee.getPosition()) + ");");
-        executeUpdate(query.toString());
+        log.log(Level.INFO, query.toString());
+        dbRep.executeUpdate(query.toString());
     }
 
     public static void insert(ScheduleRecord record) {
@@ -76,14 +73,16 @@ public class DBHelper {
                 ", secondName = " + appendField(client.getSecondName()) + ", lastName =  " +
                 appendRequiredField(client.getLastName()) + ", phone = " + appendField(client.getPhone()) +
                 ", cardNumber = " + appendRequiredField(client.getCardNumber()) + " WHERE id = " + client.getId() + ";");
-        executeUpdate(query.toString());
+        log.log(Level.INFO, query.toString());
+        dbRep.executeUpdate(query.toString());
     }
 
     public static void update(Goods goods)  {
         StringBuilder query = new StringBuilder("UPDATE goods SET code = " + appendRequiredField(goods.getCode()) +
                 ", name = " + appendField(goods.getName()) + ", price =  " + appendField(goods.getPrice()) +
                 ", amount = " + appendField(goods.getAmount()) + ";");
-        executeUpdate(query.toString());
+        log.log(Level.INFO, query.toString());
+        dbRep.executeUpdate(query.toString());
     }
 
     public static void update(Employee employee) {
@@ -91,7 +90,8 @@ public class DBHelper {
                 ", secondName = " + appendField(employee.getSecondName()) + ", lastName =  " +
                 appendRequiredField(employee.getLastName()) + ", phone = " + appendField(employee.getPhone()) +
                 ", profession = " + appendRequiredField(employee.getPosition()) + " WHERE id = " + employee.getId() + ";");
-        executeUpdate(query.toString());
+        log.log(Level.INFO, query.toString());
+        dbRep.executeUpdate(query.toString());
     }
 
     public static void update(ScheduleRecord record) {
@@ -118,161 +118,7 @@ public class DBHelper {
         return "'" + param + "'";
     }
 
-    private static void executeUpdate(String query) {
-        try {
-            // opening database connection to MySQL server
-            connection = DriverManager.getConnection(URL + "/" + DATABASE_NAME, USER, PASSWORD);
-            // getting Statement object to execute query
-            statement = connection.createStatement();
-            statement.executeUpdate(query);
 
-        } catch (SQLException sqlEx) {
-            sqlEx.printStackTrace();
-            //TODO: insert log
-        } finally {
-            try { connection.close(); } catch(SQLException se) { /*TODO: insert log*/ }
-            try { statement.close(); } catch(SQLException se) { /*TODO: insert log*/ }
-        }
-    }
-
-    private static ObservableList<? extends Base> executeQuery(String query, String tableName) {
-        ObservableList<? extends Base> dataList = null;
-        try {
-            connection = getDBConnection();
-            // getting Statement object to execute query
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery(query);
-            dataList = getObservableList(resultSet, tableName);
-        } catch (SQLException sqlEx) {
-            sqlEx.printStackTrace();
-            //TODO: insert log
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try { resultSet.close(); } catch (SQLException se) { /*TODO: insert log*/ }
-            try { connection.close(); } catch(SQLException se) { /*TODO: insert log*/ }
-            try { statement.close(); } catch(SQLException se) { /*TODO: insert log*/  }
-        }
-        return dataList;
-    }
-
-    private static void createDatabase(String databaseName) {
-        String Driver = "com.mysql.jdbc.Driver";
-        try {
-            Class.forName(Driver);
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-
-            String query = "CREATE DATABASE " + databaseName + " CHARACTER SET utf8 COLLATE utf8_general_ci;";
-
-            statement = connection.createStatement();
-            statement.executeUpdate(query);
-
-            connection = DriverManager.getConnection(URL + "/" + databaseName, USER, PASSWORD);
-            statement = connection.createStatement();
-
-            statement.executeUpdate("CREATE TABLE `" + TABLE_CARDS + "` (`id` INT NOT NULL, " +
-                    "`client_id` INT NOT NULL, PRIMARY KEY (`id`)) CHARACTER SET 'utf8' COLLATE utf8_general_ci;");
-
-            statement.executeUpdate("CREATE TABLE `" + TABLE_CLIENTS + "`(`id` INT NOT NULL, " +
-                    "`firstName` VARCHAR(45) NOT NULL, `secondName` VARCHAR(45), `lastName` VARCHAR(45) NOT NULL, " +
-                    "`phone` VARCHAR(10), PRIMARY KEY (`id`)) CHARACTER SET 'utf8' COLLATE utf8_general_ci;");
-
-            statement.executeUpdate("CREATE TABLE `" + TABLE_EMPLOYEES + "` (`id` INT NOT NULL, " +
-                    "`firstName` VARCHAR(45) NOT NULL, `secondName` VARCHAR(45), `lastName` VARCHAR(45) NOT NULL, " +
-                    "`phone` VARCHAR(10), `profession` VARCHAR(45) NOT NULL, PRIMARY KEY (`id`)) CHARACTER SET 'utf8' " +
-                    "COLLATE utf8_general_ci;");
-
-            statement.executeUpdate("CREATE TABLE `" + TABLE_ORDERS + "` (`id` INT NOT NULL, " +
-                    "`client_id` INT NOT NULL, `employee_id` INT, `isPaid` BOOL, `price` DECIMAL(9,2), " +
-                    "`date` TIMESTAMP, PRIMARY KEY (`id`)) CHARACTER SET 'utf8' COLLATE utf8_general_ci;");
-
-            statement.executeUpdate("CREATE TABLE `" + TABLE_PRODUCTS + "` (`id` INT NOT NULL, " +
-                    "`name` VARCHAR(45) NOT NULL, `price` DECIMAL(9,2), PRIMARY KEY (`id`)) CHARACTER SET 'utf8'" +
-                    "COLLATE utf8_general_ci;");
-
-            statement.executeUpdate("CREATE TABLE `" + TABLE_RENTERS + "` (`id` INT NOT NULL," +
-                    " `firstName` VARCHAR(45) NOT NULL, `secondName` VARCHAR(45), `lastName` VARCHAR(45) NOT NULL, " +
-                    "`phone` VARCHAR(10), `rent` DECIMAL(9,2), PRIMARY KEY (`id`)) CHARACTER SET 'utf8' " +
-                    "COLLATE utf8_general_ci;");
-
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            //TODO: insert log
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            //TODO: insert log
-        } finally {
-            try {connection.close();} catch (SQLException e) { /*TODO: insert log*/ }
-            try {statement.close();} catch (SQLException e) { /*TODO: insert log*/ }
-        }
-    }
-
-    protected static Connection getDBConnection()throws Exception {
-        Class.forName("com.mysql.jdbc.Driver");
-        java.util.Properties properties = new java.util.Properties();
-        properties.put("user", USER);
-        properties.put("password", PASSWORD);
-
-        /* настройки указывающие о необходимости конвертировать данные из Unicode
-	    в UTF-8, который используется в нашей таблице для хранения данных */
-
-        properties.setProperty("useUnicode", "true");
-        properties.setProperty("characterEncoding", "UTF-8");
-        return (DriverManager.getConnection("jdbc:mysql://localhost:3306/iris_db",
-                properties));
-    }
-
-
-    private static ObservableList<? extends Base> getObservableList(ResultSet set, String tableName) {
-        try {
-            switch (tableName.toLowerCase()) {
-                case "clients":
-                    ObservableList<Client> clientData = FXCollections.observableArrayList();
-                    return fillClients(set, clientData);
-                case "employees":
-                    ObservableList<Employee> employeeData = FXCollections.observableArrayList();
-                    return fillEmployees(set, employeeData);
-                case "goods":
-                    ObservableList<Goods> goodsData = FXCollections.observableArrayList();
-                    return fillGoods(set, goodsData);
-                case "orders":
-                    ObservableList<ScheduleRecord> scheduleRecordData = FXCollections.observableArrayList();
-                    return fillRecords(set, scheduleRecordData);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        throw new IllegalArgumentException();
-    }
-
-    private static ObservableList<ScheduleRecord> fillRecords(ResultSet set, ObservableList<ScheduleRecord> scheduleRecordData) throws SQLException {
-        while (set.next()) {
-            scheduleRecordData.add(new ScheduleRecord(set.getString(1), set.getString(2), set.getString(3)));
-        }
-        return scheduleRecordData;
-    }
-
-    private static ObservableList<Goods> fillGoods(ResultSet set, ObservableList<Goods> goodsData) throws SQLException {
-        while (set.next()) {
-            goodsData.add(new Goods(set.getString(1), set.getString(2), set.getString(3), set.getString(4)));
-        }
-        return goodsData;
-    }
-
-    private static ObservableList<Employee> fillEmployees(ResultSet set, ObservableList<Employee> employeeData) throws SQLException {
-        while (set.next()) {
-            employeeData.add(new Employee(set.getString(2), set.getString(3), set.getString(4), set.getString(5),
-                    set.getString(6)));
-        }
-        return employeeData;
-    }
-
-    private static ObservableList<Client> fillClients(ResultSet set, ObservableList<Client> clientData) throws SQLException {
-        while (set.next()) {
-            clientData.add(new Client(set.getString(2), set.getString(3), set.getString(4), set.getString(5)));
-        }
-        return clientData;
-    }
 
 }
 
---
+++ src/main/java/ua/com/jdev/dbase/DBRepository.java
@@ -0,0 +1,182 @@
+package ua.com.jdev.dbase;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import ua.com.jdev.model.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+
+/**
+ * Created by Jurimik on 26.01.2016.
+ */
+public class DBRepository {
+
+    private static Connection connection;
+    private static Statement statement;
+    private static ResultSet resultSet;
+
+    private static final String URL = "jdbc:mysql://localhost:3306";
+    private static final String USER = "iris";
+    private static final String PASSWORD = "1234qaz";
+
+    private static final Logger log = Logger.getLogger(DBRepository.class);
+
+    void executeUpdate(String query) {
+        try {
+            // opening database connection to MySQL server
+            connection = DriverManager.getConnection(URL + "/" + DBHelper.DATABASE_NAME, USER, PASSWORD);
+            // getting Statement object to execute query
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+
+        } catch (SQLException sqlEx) {
+            log.log(Level.WARN, query, sqlEx);
+        } finally {
+            try { connection.close(); } catch(SQLException se) { log.log(Level.WARN, connection.toString(), se); }
+            try { statement.close(); } catch(SQLException se) { log.log(Level.WARN, statement.toString(), se); }
+        }
+    }
+
+    ObservableList<? extends Base> executeQuery(String query, String tableName) {
+        ObservableList<? extends Base> dataList = null;
+        try {
+            connection = getDBConnection();
+            // getting Statement object to execute query
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+            dataList = getObservableList(resultSet, tableName);
+        } catch (SQLException sqlEx) {
+            log.log(Level.WARN, query + ", tableName: " + tableName, sqlEx);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try { resultSet.close(); } catch (SQLException se) { log.log(Level.WARN, resultSet.toString(), se); }
+            try { connection.close(); } catch(SQLException se) { log.log(Level.WARN, connection.toString(), se); }
+            try { statement.close(); } catch(SQLException se) { log.log(Level.WARN, statement.toString(), se);  }
+        }
+        return dataList;
+    }
+
+    private static void createDatabase(String databaseName) {
+        String Driver = "com.mysql.jdbc.Driver";
+        try {
+            Class.forName(Driver);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+
+            String query = "CREATE DATABASE " + databaseName + " CHARACTER SET utf8 COLLATE utf8_general_ci;";
+
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+
+            connection = DriverManager.getConnection(URL + "/" + databaseName, USER, PASSWORD);
+            statement = connection.createStatement();
+
+            statement.executeUpdate("CREATE TABLE `" + DBHelper.TABLE_CARDS + "` (`id` INT NOT NULL, " +
+                    "`client_id` INT NOT NULL, PRIMARY KEY (`id`)) CHARACTER SET 'utf8' COLLATE utf8_general_ci;");
+
+            statement.executeUpdate("CREATE TABLE `" + DBHelper.TABLE_CLIENTS + "`(`id` INT NOT NULL, " +
+                    "`firstName` VARCHAR(45) NOT NULL, `secondName` VARCHAR(45), `lastName` VARCHAR(45) NOT NULL, " +
+                    "`phone` VARCHAR(10), PRIMARY KEY (`id`)) CHARACTER SET 'utf8' COLLATE utf8_general_ci;");
+
+            statement.executeUpdate("CREATE TABLE `" + DBHelper.TABLE_EMPLOYEES + "` (`id` INT NOT NULL, " +
+                    "`firstName` VARCHAR(45) NOT NULL, `secondName` VARCHAR(45), `lastName` VARCHAR(45) NOT NULL, " +
+                    "`phone` VARCHAR(10), `profession` VARCHAR(45) NOT NULL, PRIMARY KEY (`id`)) CHARACTER SET 'utf8' " +
+                    "COLLATE utf8_general_ci;");
+
+            statement.executeUpdate("CREATE TABLE `" + DBHelper.TABLE_ORDERS + "` (`id` INT NOT NULL, " +
+                    "`client_id` INT NOT NULL, `employee_id` INT, `isPaid` BOOL, `price` DECIMAL(9,2), " +
+                    "`date` TIMESTAMP, PRIMARY KEY (`id`)) CHARACTER SET 'utf8' COLLATE utf8_general_ci;");
+
+            statement.executeUpdate("CREATE TABLE `" + DBHelper.TABLE_PRODUCTS + "` (`id` INT NOT NULL, " +
+                    "`name` VARCHAR(45) NOT NULL, `price` DECIMAL(9,2), PRIMARY KEY (`id`)) CHARACTER SET 'utf8'" +
+                    "COLLATE utf8_general_ci;");
+
+            statement.executeUpdate("CREATE TABLE `" + DBHelper.TABLE_RENTERS + "` (`id` INT NOT NULL," +
+                    " `firstName` VARCHAR(45) NOT NULL, `secondName` VARCHAR(45), `lastName` VARCHAR(45) NOT NULL, " +
+                    "`phone` VARCHAR(10), `rent` DECIMAL(9,2), PRIMARY KEY (`id`)) CHARACTER SET 'utf8' " +
+                    "COLLATE utf8_general_ci;");
+
+        } catch (SQLException ex) {
+            log.log(Level.WARN, databaseName, ex);
+        } catch (ClassNotFoundException e) {
+            log.log(Level.WARN, e);
+        } finally {
+            try {connection.close();} catch (SQLException e) { log.log(Level.WARN, connection.toString(), e); }
+            try {statement.close();} catch (SQLException e) { log.log(Level.WARN, statement.toString(), e); }
+        }
+    }
+
+    private static Connection getDBConnection()throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+        java.util.Properties properties = new java.util.Properties();
+        properties.put("user", USER);
+        properties.put("password", PASSWORD);
+
+        /* настройки указывающие о необходимости конвертировать данные из Unicode
+	    в UTF-8, который используется в нашей таблице для хранения данных */
+
+        properties.setProperty("useUnicode", "true");
+        properties.setProperty("characterEncoding", "UTF-8");
+        return (DriverManager.getConnection("jdbc:mysql://localhost:3306/iris_db",
+                properties));
+    }
+
+
+    private static ObservableList<? extends Base> getObservableList(ResultSet set, String tableName) {
+        try {
+            switch (tableName.toLowerCase()) {
+                case "clients":
+                    ObservableList<Client> clientData = FXCollections.observableArrayList();
+                    return fillClients(set, clientData);
+                case "employees":
+                    ObservableList<Employee> employeeData = FXCollections.observableArrayList();
+                    return fillEmployees(set, employeeData);
+                case "goods":
+                    ObservableList<Goods> goodsData = FXCollections.observableArrayList();
+                    return fillGoods(set, goodsData);
+                case "orders":
+                    ObservableList<ScheduleRecord> scheduleRecordData = FXCollections.observableArrayList();
+                    return fillRecords(set, scheduleRecordData);
+            }
+        } catch (SQLException e) {
+            log.log(Level.WARN, tableName, e);
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private static ObservableList<ScheduleRecord> fillRecords(ResultSet set, ObservableList<ScheduleRecord> scheduleRecordData) throws SQLException {
+        while (set.next()) {
+            scheduleRecordData.add(new ScheduleRecord(set.getString(1), set.getString(2), set.getString(3)));
+        }
+        return scheduleRecordData;
+    }
+
+    private static ObservableList<Goods> fillGoods(ResultSet set, ObservableList<Goods> goodsData) throws SQLException {
+        while (set.next()) {
+            goodsData.add(new Goods(set.getString(1), set.getString(2), set.getString(3), set.getString(4)));
+        }
+        return goodsData;
+    }
+
+    private static ObservableList<Employee> fillEmployees(ResultSet set, ObservableList<Employee> employeeData) throws SQLException {
+        while (set.next()) {
+            employeeData.add(new Employee(set.getString(2), set.getString(3), set.getString(4), set.getString(5),
+                    set.getString(6)));
+        }
+        return employeeData;
+    }
+
+    private static ObservableList<Client> fillClients(ResultSet set, ObservableList<Client> clientData) throws SQLException {
+        while (set.next()) {
+            clientData.add(new Client(set.getString(2), set.getString(3), set.getString(4), set.getString(5)));
+        }
+        return clientData;
+    }
+}
