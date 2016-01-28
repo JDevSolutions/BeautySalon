d72544f1405bf57c001cbd27522b50bfc477d7a0
driversti <driversti@gmail.com>
2016-01-28 13:01:08 +0200

    Удалил закомментированное

--- src/main/java/ua/com/jdev/model/BaseClass.java
+++ src/main/java/ua/com/jdev/model/BaseClass.java
@@ -1,18 +1,20 @@
 package ua.com.jdev.model;
 
+import javafx.beans.property.StringProperty;
+
 public abstract class BaseClass {
 
-//    protected StringProperty id;
-//
-//    public void setId(String id) {
-//        this.id.set(id);
-//    }
-//
-//    public String getId() {
-//        return id.get();
-//    }
-//
-//    public StringProperty idProperty() {
-//        return id;
-//    }
+    protected StringProperty id;
+
+    public void setId(String id) {
+        this.id.set(id);
+    }
+
+    public String getId() {
+        return id.get();
+    }
+
+    public StringProperty idProperty() {
+        return id;
+    }
 }
