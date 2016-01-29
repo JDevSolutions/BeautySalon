08abafa21c4a3559b62ac63940d59456a1093d5d
JuriMik <jurimik@yandex.ua>
2016-01-29 22:04:49 +0200

    Change class Constants to the actual state

--- src/main/java/ua/com/jdev/dbase/Constants.java
+++ src/main/java/ua/com/jdev/dbase/Constants.java
@@ -5,14 +5,63 @@ package ua.com.jdev.dbase;
  */
 public class Constants {
     static final String DATABASE_NAME = "iris_db";
+
     static final String TABLE_EMPLOYEES = "employees";
-    static final String[] TABLE_EMPLOYEES_COLUMNS = {"id", "firstName", "secondName", "lastName", "phone", "profession"};
+    static final String[] TABLE_EMPLOYEES_COLUMNS = {
+            "`id` INT NOT NULL AUTO_INCREMENT",
+            "`firstName` VARCHAR(45) NOT NULL",
+            "`secondName` VARCHAR(45)",
+            "`lastName` VARCHAR(45) NOT NULL",
+            "`phone` VARCHAR(10)",
+            "`profession` VARCHAR(45) NOT NULL",
+            "`isActive` BOOL"
+    };
+    static final String TABLE_EMPLOYEES_PRIMARY_KEY = "id";
+
     static final String TABLE_ORDERS = "orders";
-    static final String[] TABLE_ORDERS_COLUMNS = {"id", "client_id", "employee_id", "time", "price"};
-    static final String TABLE_PRODUCTS = "products";
-    static final String[] TABLE_PRODUCTS_COLUMNS = {"id", "code", "name", "price", "count"};
+    static final String[] TABLE_ORDERS_COLUMNS = {
+            "`id` INT NOT NULL AUTO_INCREMENT",
+            "`client_id` INT NOT NULL",
+            "`employee_id` INT NOT NULL",
+            "`isPaid` BOOL",
+            "`date` TIMESTAMP",
+            "`price` DECIMAL(9,2)",
+            "`isActive` BOOL"
+    };
+    static final String TABLE_ORDERS_PRIMARY_KEY = "id";
+
+    static final String TABLE_GOODS = "goods";
+    static final String[] TABLE_PRODUCTS_COLUMNS = {
+            "`id` INT NOT NULL AUTO_INCREMENT",
+            "`code` VARCHAR(10)",
+            "`name` VARCHAR(45) NOT NULL",
+            "`price` DECIMAL(9,2)",
+            "`amount` INT NOT NULL"
+    };
+    static final String TABLE_GOODS_PRIMARY_KEY = "id";
+
     static final String TABLE_RENTERS = "renters";
-    static final String[] TABLE_RENTER_COLUMNS = {"id", "firstName", "secondName", "lastName", "phone", "rent"};
+    static final String[] TABLE_RENTER_COLUMNS = {
+            "`id` INT NOT NULL AUTO_INCREMENT",
+            "`firstName` VARCHAR(45) NOT NULL",
+            "`secondName` VARCHAR(45)",
+            "`lastName` VARCHAR(45) NOT NULL",
+            "`phone` VARCHAR(10)",
+            "`rent` DECIMAL(9,2)",
+            "`isActive` BOOL",
+    };
+    static final String TABLE_RENTERS_PRIMARY_KEY = "id";
+
     static final String TABLE_CLIENTS = "clients";
-    static final String[] TABLE_CLIENTS_COLUMNS = {"id", "firstName", "secondName", "lastName", "phone", "card_number"};
+    static final String[] TABLE_CLIENTS_COLUMNS = {
+            "`id` INT NOT NULL AUTO_INCREMENT",
+            "`firstName` VARCHAR(45) NOT NULL",
+            "`secondName` VARCHAR(45)",
+            "`lastName` VARCHAR(45) NOT NULL",
+            "`phone` VARCHAR(13)",
+            "`cardNumber` VARCHAR(7)",
+            "`isActive` BOOL"
+    };
+    static final String TABLE_CLIENTS_PRIMARY_KEY = "id";
+
 }
