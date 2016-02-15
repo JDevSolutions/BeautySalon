4b930e2deac5ad7c14ef813f56ed8f7f3d3b0ce6
driversti <driversti@gmail.com>
2016-02-15 19:59:57 +0200

    Создан новый диалог для продажи товаров. Логика пока не реализована

--- src/main/java/ua/com/jdev/view/dialogs/GoodsSaleDialogController.java
+++ src/main/java/ua/com/jdev/view/dialogs/GoodsSaleDialogController.java
@@ -1,25 +1,10 @@
 package ua.com.jdev.view.dialogs;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import ua.com.jdev.dbase.DBHelper;
 import ua.com.jdev.model.Goods;
 
 public class GoodsSaleDialogController {
-
-    @FXML
-    private TextField codeGoodsField;
-    @FXML
-    private TextField nameGoodsField;
-    @FXML
-    private TextField priceGoodsField;
-    @FXML
-    private TextField amountGoodsField;
-    @FXML
-    private TextField totalGoodsField;
 
     private Stage dialogStage;
     private Goods goods;
@@ -50,13 +35,6 @@ public class GoodsSaleDialogController {
     public void setGoods(Goods goods) {
         this.goods = goods;
 
-        codeGoodsField.setText(goods.getCode());
-        codeGoodsField.setEditable(false);// not working
-        nameGoodsField.setText(goods.getName());
-        nameGoodsField.setEditable(false);// not working
-        priceGoodsField.setText(String.valueOf(goods.getPrice()));
-        amountGoodsField.setText(String.valueOf(goods.getAmount()));
-        totalGoodsField.setText(String.valueOf(goods.getPrice() * goods.getAmount()));
     }
 
     /**
@@ -73,17 +51,11 @@ public class GoodsSaleDialogController {
      */
     @FXML
     private void handleOk() {
-        if (isInputValid()) {
-            goods.setCode(codeGoodsField.getText());
-            goods.setName(nameGoodsField.getText());
-            goods.setPrice(Double.parseDouble(priceGoodsField.getText()));
-            goods.setAmount(Integer.parseInt(amountGoodsField.getText()));
 
-            DBHelper.sale(goods, Integer.parseInt(amountGoodsField.getText()), Double.parseDouble(priceGoodsField.getText()));
+            //DBHelper.sale(goods);
 
             okClicked = true;
             dialogStage.close();
-        }
     }
 
     /**
@@ -92,60 +64,5 @@ public class GoodsSaleDialogController {
     @FXML
     private void handleCancel() {
         dialogStage.close();
-    }
-
-    /**
-     * Validates the user input in the text fields.
-     *
-     * @return true if the input is valid
-     */
-    private boolean isInputValid() {
-        String errorMessage = "";
-
-        if (priceGoodsField.getText() == null || priceGoodsField.getText().length() == 0) {
-            errorMessage += "Цена товара содержит ошибку!\n";
-        }
-        if (amountGoodsField.getText() == null || amountGoodsField.getText().length() == 0) {
-            errorMessage += "Количество товаров содержит ошибку\n";
-        } else {
-            // Проверка на наличие любых символов кроме цифр
-            try {
-                Integer.parseInt(amountGoodsField.getText());
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-                errorMessage += "Количество товаров должно содержать только цифры!\n";
-            }
-        }
-
-        if (errorMessage.length() == 0) {
-            return true;
-        } else {
-            // Show the error message.
-            Alert alert = new Alert(AlertType.ERROR);
-            alert.initOwner(dialogStage);
-            alert.setTitle("Ошибки в полях товара");
-            alert.setHeaderText("Исправьте ошибки в полях, пожалуйста");
-            alert.setContentText(errorMessage);
-
-            alert.showAndWait();
-
-            return false;
-        }
-    }
-
-    public TextField getCodeGoodsField() {
-        return codeGoodsField;
-    }
-
-    public TextField getNameGoodsField() {
-        return nameGoodsField;
-    }
-
-    public TextField getPriceGoodsField() {
-        return priceGoodsField;
-    }
-
-    public TextField getAmountGoodsField() {
-        return amountGoodsField;
     }
 }
--- src/main/java/ua/com/jdev/view/dialogs/OrderAddDialogController.java
+++ src/main/java/ua/com/jdev/view/dialogs/OrderAddDialogController.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import ua.com.jdev.adapter.PersonMaker;
 import ua.com.jdev.dbase.DBHelper;
 import ua.com.jdev.dbase.DBRepository;
 import ua.com.jdev.model.Order;
